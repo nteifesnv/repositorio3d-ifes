a8e61d47db22fe2d604d6b786a1e720926684226
NTE-IFES-NV <nte3d.ifes.nv@gmail.com>
2026-03-08 13:00:41 -0300

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,34 @@
+fetch("modelos.json")
+.then(response => response.json())
+.then(data => {
 
+const gallery = document.getElementById("gallery");
+
+data.forEach(modelo => {
+
+const card = document.createElement("div");
+card.className = "card";
+
+card.innerHTML = `
+<img src="${modelo.imagem}">
+
+<div class="card-content">
+
+<h3>${modelo.nome}</h3>
+
+<p>${modelo.descricao}</p>
+
+<p><strong>Categoria:</strong> ${modelo.categoria}</p>
+
+<a class="download" href="${modelo.download}" target="_blank">
+Download
+</a>
+
+</div>
+`;
+
+gallery.appendChild(card);
+
+});
+
+});
